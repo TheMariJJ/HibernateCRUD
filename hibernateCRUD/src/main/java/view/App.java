42842f70656d6352bc60e1249327caa7fe5af5c0
TheMariJJ <maridelima97@gmail.com>
2018-05-23 20:23:50 -0300

    exemplo

---
+++ hibernateCRUD/src/main/java/view/App.java
@@ -0,0 +1,19 @@
+package view;
+
+import javax.swing.JFrame;
+
+import controller.ClienteJpaDAO;
+import model.Cliente;
+
+public class App {
+	public static void main( String[] args )
+    {
+    	 Cliente cliente = new Cliente();
+         cliente.setCpf("372.468.423-53");
+         cliente.setId(1);
+         cliente.setNome("Jeferson Roberto de Lima");
+         cliente.setRg("52.563.456-5");
+         ClienteJpaDAO.getInstance().merge(cliente);
+         System.out.println("Objetos salvo com sucesso!!!");
+    }
+}
